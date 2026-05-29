2b8eaaa7c15718339aac9e7dc8079a50a6906302
ricardocorbetta <65521563+ricardocorbetta@users.noreply.github.com>
2026-05-28 23:17:47 -0300

    Create supabase.js

---
+++ src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const SUPA_URL = "https://imkmosifqxzbtqgzssst.supabase.co"
+const SUPA_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlta21vc2lmcXh6YnRxZ3pzc3N0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxODk4NTUsImV4cCI6MjA5NDc2NTg1NX0.5gtCs8Yv3vDSrKxAmXSr3zjWJ5HjimCKejfO-XrHPss"
+
+export const supabase = createClient(SUPA_URL, SUPA_KEY)
