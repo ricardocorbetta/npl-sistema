7eb1ce3f5abbcd5e1f512639edf0e14d274ef3da
ricardocorbetta <65521563+ricardocorbetta@users.noreply.github.com>
2026-06-21 10:58:13 -0300

    Update Obras.jsx

--- src/Obras.jsx
+++ src/Obras.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useCallback } from "react";
 import { supabase } from "./supabase.js";
 
 const SUPA_URL = "https://imkmosifqxzbtqgzssst.supabase.co/rest/v1";
-const ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlta21vc2lmcXh6YnRxZ3pzc3N0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYzODMxMjMsImV4cCI6MjA2MTk1OTEyM30.3L4YPsHnFWEBFLlJEZYFoX4wCYwZiHGHERgKCbUqiH4";
+const ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlta21vc2lmcXh6YnRxZ3pzc3N0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxODk4NTUsImV4cCI6MjA5NDc2NTg1NX0.5gtCs8Yv3vDSrKxAmXSr3zjWJ5HjimCKejfO-XrHPss";
 
 async function getToken() {
   const { data: { session } } = await supabase.auth.getSession();
