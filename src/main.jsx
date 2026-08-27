0aea5e314590ed1af56eb2a6c891933b6f8d2b06
ricardocorbetta <65521563+ricardocorbetta@users.noreply.github.com>
2026-08-27 14:42:44 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,5 @@ import ReactDOM from 'react-dom/client'
 import Root from './Root.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>
+  <Root />
 )
